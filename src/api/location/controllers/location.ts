804ee39514f8a1a2a37e2145f2eb1720781ad42a
AbdelSalam Selim <abdelsalam.selim@flairstech.com>
2023-03-23 09:57:51 -0700

    Added Services and Refcatored Code to Follow SOLID and DRY

--- src/api/location/controllers/location.ts
+++ src/api/location/controllers/location.ts
@@ -10,79 +10,41 @@ export default factories.createCoreController('api::location.location', ({ strap
     isItRaining: async (ctx) => {
         const url = process.env.Weather_Url;
         const appid = process.env.Weather_App_Id;
-        try {
-            let locationWeather: weather = {
-                lon: "",
-                lat: "",
-                weatherMain: "",
-                weatherDescription: "",
-                isItRaining: false
-            };
-            const reqCtx = strapi.requestContext.get();
-            reqCtx.url = reqCtx.url.toLowerCase();
-            const qs1 = (reqCtx.url.trim().split("/api/v1/isitraining?")[1]).split("&")[0];
-            const qs2 = (reqCtx.url.trim().split("/api/v1/isitraining?")[1]).split("&")[1];
-            const feild1 = qs1.split('=');
-            const feild2 = qs2.split('=');
 
-            if (feild1[0] == 'lon' && feild2[0] == 'lat') {
-                var lon = feild1[1];
-                var lat = feild2[1];
-                await axios.post(`${url}lon=${lon}&lat=${lat}&appid=${appid}`).then(
-                    (response) => {
-                        var weatherResponse = response.data.weather[0];
-                        if (weatherResponse.main.includes("Rain") || weatherResponse.description.includes("rain")) {
-                            locationWeather.isItRaining = true;
-                            locationWeather.lon = lon;
-                            locationWeather.lat = lat;
-                            locationWeather.weatherMain = weatherResponse.main;
-                            locationWeather.weatherDescription = weatherResponse.description;
-                        }
-                        else {
-                            locationWeather.isItRaining = false;
-                            locationWeather.lon = lon;
-                            locationWeather.lat = lat;
-                            locationWeather.weatherMain = weatherResponse.main;
-                            locationWeather.weatherDescription = weatherResponse.description;
-                        }
-                    },
-                    (error) => { console.log(error); }
-                );
-            }
-            else if (feild1[0] == 'lat' && feild2[0] == 'lon') {
-                var lat = feild1[1];
-                var lon = feild2[1];
-                await axios.post(`${url}lon=${lon}&lat=${lat}&appid=${appid}`).then(
-                    (response) => {
-                        var weatherResponse = response.data.weather[0];
-                        if (weatherResponse.main.includes("Rain") || weatherResponse.description.includes("rain")) {
-                            locationWeather.isItRaining = true;
-                            locationWeather.lon = lon;
-                            locationWeather.lat = lat;
-                            locationWeather.weatherMain = weatherResponse.main;
-                            locationWeather.weatherDescription = weatherResponse.description;
-                        }
-                        else {
-                            locationWeather.isItRaining = false;
-                            locationWeather.lon = lon;
-                            locationWeather.lat = lat;
-                            locationWeather.weatherMain = weatherResponse.main;
-                            locationWeather.weatherDescription = weatherResponse.description;
-                        }
-                    },
-                    (error) => { console.log(error); }
-                );
+        try {
+            //extracting query strings
+            const requestContext = strapi.requestContext.get();
+            var result = await strapi
+                .service('api::location.location')
+                .extractQueryStrings(requestContext.url.toLowerCase(), "isitraining");
+
+            //Initiate request to Weather API
+            var { data } = await axios.post(`${url}lon=${result.lon}&lat=${result.lat}&appid=${appid}`);
+
+            var response = data.weather[0];
+
+            if (response.main.includes("Rain") || response.description.includes("rain")) {
+                var locationWeather: weather = await strapi
+                    .service('api::location.location')
+                    .mapLocationWeather(true, result, response.main, response.description);
+                return locationWeather;
             }
             else {
+                var locationWeather: weather = await strapi
+                    .service('api::location.location')
+                    .mapLocationWeather(false, result, response.main, response.description);
 
-                throw new Error("Invalid Parameters // Longitude or latitude invalid");
-
+                return locationWeather;
             }
 
-            return locationWeather;
 
-        } catch (err) {
-            console.log(err);
+        }
+        catch (err) {
+            //Maintain Response Code
+            console.log(JSON.stringify(ctx.response));
+            ctx.response.status = 400;
+            ctx.response.body = { error: { status: 400, message: 'Bad Request - Invalid Parameters' } }
+
         }
     },
 
@@ -92,82 +54,51 @@ export default factories.createCoreController('api::location.location', ({ strap
         const appid = process.env.Weather_App_Id;
         let activityType: string = '';
         try {
+            //preparing response object
             let weatherActivity = { recommendedActivity: "", type: "", weatherDescription: "" }
-            const reqCtx = strapi.requestContext.get();
-            reqCtx.url = reqCtx.url.toLowerCase();
-            const qs1 = (reqCtx.url.trim().split("/api/v1/whattodo?")[1]).split("&")[0];
-            const qs2 = (reqCtx.url.trim().split("/api/v1/whattodo?")[1]).split("&")[1];
-            const feild1 = qs1.split('=');
-            const feild2 = qs2.split('=');
 
-            if (feild1[0] == 'lon' && feild2[0] == 'lat') {
-                var lon = feild1[1];
-                var lat = feild2[1];
-                await axios.post(`${weatherUrl}lon=${lon}&lat=${lat}&appid=${appid}`).then(
-                    (response) => {
-                        var weatherResponse = response.data.weather[0];
-                        weatherActivity.weatherDescription = weatherResponse.description;
-                        var weatherResponseDescription = weatherResponse.description.toLowerCase();
-                        if (weatherResponseDescription.includes('rain') || weatherResponseDescription.includes('cloud')) {
-                            activityType = 'cooking';
-                            weatherActivity.type = 'cooking';
-                        }
-                        else if (weatherResponseDescription.includes('sun') || weatherResponseDescription.includes('warm')) {
-                            activityType = 'relaxation';
-                            weatherActivity.type = 'relaxation';
-                        }
-                        else {
-                            activityType = 'social';
-                            weatherActivity.type = 'social';
-                        }
-                    },
-                    (error) => { console.log(error); }
-                );
-                await axios.get(`${activityUrl}type=${activityType}`).then((res) => {
-                    weatherActivity.recommendedActivity = res.data.activity;
-                }, (err) => {
-                    console.log(err)
-                });
+            //extracting query strings
+            const requestContext = strapi.requestContext.get();
+            var result = await strapi
+                .service('api::location.location')
+                .extractQueryStrings(requestContext.url.toLowerCase(), "whattodo");
 
-            }
-            else if (feild1[0] == 'lat' && feild2[0] == 'lon') {
-                var lat = feild1[1];
-                var lon = feild2[1];
-                await axios.post(`${weatherUrl}lon=${lon}&lat=${lat}&appid=${appid}`).then(
-                    (response) => {
-                        var weatherResponse = response.data.weather[0];
-                        weatherActivity.weatherDescription = weatherResponse.description;
-                        var weatherResponseDescription = weatherResponse.description.toLowerCase();
-                        if (weatherResponseDescription.includes('rain') || weatherResponseDescription.includes('cloud')) {
-                            activityType = 'cooking';
-                            weatherActivity.type = 'cooking';
-                        }
-                        else if (weatherResponseDescription.includes('sun') || weatherResponseDescription.includes('warm')) {
-                            activityType = 'relaxation';
-                            weatherActivity.type = 'relaxation';
-                        }
-                        else {
-                            activityType = 'social';
-                            weatherActivity.type = 'social';
-                        }
-                    },
-                    (error) => { console.log(error); }
-                );
-                await axios.get(`${activityUrl}type=${activityType}`).then((res) => {
-                    weatherActivity.recommendedActivity = res.data.activity;
-                }, (err) => {
-                    console.log(err)
-                });
-            }
-            else {
+            //Initiate request to Weather API
+            await axios.post(`${weatherUrl}lon=${result.lon}&lat=${result.lon}&appid=${appid}`).then(
+                (response) => {
+                    var weatherResponse = response.data.weather[0];
+                    weatherActivity.weatherDescription = weatherResponse.description;
+                    var weatherResponseDescription = weatherResponse.description.toLowerCase();
+                    if (weatherResponseDescription.includes('rain') || weatherResponseDescription.includes('cloud')) {
+                        activityType = 'cooking';
+                        weatherActivity.type = 'cooking';
+                    }
+                    else if (weatherResponseDescription.includes('sun') || weatherResponseDescription.includes('warm')) {
+                        activityType = 'relaxation';
+                        weatherActivity.type = 'relaxation';
+                    }
+                    else {
+                        activityType = 'social';
+                        weatherActivity.type = 'social';
+                    }
+                },
+                (error) => { console.log(error); });
 
-                throw new Error("Invalid Parameters // Longitude or latitude invalid");
+            //Initiate request to Activity API
+            await axios.get(`${activityUrl}type=${activityType}`).then((res) => {
+                weatherActivity.recommendedActivity = res.data.activity;
+            }, (err) => {
+                console.log(err)
+            });
 
-            }
+
             return weatherActivity;
         }
         catch (err) {
-            console.log(err);
+            //Maintain Response Code
+            console.log(JSON.stringify(ctx.response));
+            ctx.response.status = 400;
+            ctx.response.body = { error: { status: 400, message: 'Bad Request - Invalid Parameters' } }
         }
     }
 }));
--- src/api/location/services/location.ts
+++ src/api/location/services/location.ts
@@ -3,5 +3,85 @@
  */
 
 import { factories } from '@strapi/strapi';
+import { weather } from '../model/weather';
 
-export default factories.createCoreService('api::location.location');
+export default factories.createCoreService('api::location.location', ({ strapi }) => ({
+
+    extractQueryStrings: async (url: any, endpointName: string) => {
+        try {
+            let result = { lon: 0, lat: 0 };
+
+            const urlQuery: string[] = (url.trim().split(`/api/v1/${endpointName}?`)[1]).split("&");
+
+            if (urlQuery.length > 2 || urlQuery.length < 2) {
+                throw new Error("Invalid longtitude or latitude or both" + "Line: 17 s");
+            }
+            const qs1 = urlQuery[0];
+            const qs2 = urlQuery[1];
+
+            const feild1 = qs1.split('=');
+            const feild2 = qs2.split('=');
+
+            if (feild1.length > 2 || feild2.length > 2 || feild1.length < 2 || feild2.length < 2) {
+                throw new Error("Invalid longtitude or latitude or both" + "Line: 26 s");
+            }
+
+
+            if (feild1[0] == 'lon' && feild2[0] == 'lat') {
+                let lon: number = + feild1[1];
+                let lat: number = + feild2[1];
+
+                if (isNaN(lat) || isNaN(lon)) {
+                    throw new Error("Invalid Longtitude or latitude or both" + "Line: 35 s");
+                }
+                result.lat = lat;
+                result.lon = lon;
+                if (result.lat === 0 || result.lon === 0) { throw new Error("Invalid Longtitude or latitude or both" + "Line: 39 s"); }
+                else {
+                    return result;
+                };
+            }
+            else if (feild1[0] == 'lat' && feild2[0] == 'lon') {
+                let lat: number = + feild1[1];
+                let lon: number = + feild2[1];
+
+                if (isNaN(lat) || isNaN(lon)) {
+                    throw new Error("Invalid Longtitude or latitude or both" + "Line: 50 s");
+                }
+
+                result.lat = lat;
+                result.lon = lon;
+                if (result.lat === 0 || result.lon === 0) { throw new Error("Invalid Longtitude or latitude or both" + "Line: 55 s"); }
+
+            }
+            else {
+                throw new Error("Invalid Parameters");
+            }
+
+            return result;
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    mapLocationWeather: async (isRaining: boolean, result: any, weatherMain: string, weatherDescription: string) => {
+        let locationWeather: weather = {
+            lon: result.lon.toString(),
+            lat: result.lat.toString(),
+            weatherMain: weatherMain,
+            weatherDescription: weatherDescription,
+            isItRaining: true
+        };
+        if (!isRaining) {
+            locationWeather.isItRaining = false;
+        }
+        else {
+            locationWeather.isItRaining = true;
+        }
+
+        return locationWeather;
+    },
+
+}));
